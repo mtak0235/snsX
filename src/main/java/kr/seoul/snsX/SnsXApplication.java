54ef613fefb10613a02a1593ac44fad2776530a4
Wonwoo Cho <greenaloud@gmail.com>
2022-03-29 13:06:49 +0900

    initial commit

---
+++ src/main/java/kr/seoul/snsX/SnsXApplication.java
@@ -0,0 +1,13 @@
+package kr.seoul.snsX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/seoul/snsX/SnsXApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.seoul.snsX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
