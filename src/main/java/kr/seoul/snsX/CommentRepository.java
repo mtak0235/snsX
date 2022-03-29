461f2bc5df58bc35e2f17e06470767232b9932ba
Wonwoo Cho <greenaloud@gmail.com>
2022-03-29 20:58:08 +0900

    delete Repositories

--- src/main/java/kr/seoul/snsX/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.seoul.snsX;
-
-import kr.seoul.snsX.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/kr/seoul/snsX/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.seoul.snsX;
-
-import kr.seoul.snsX.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
