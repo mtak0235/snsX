3e392c5ecbb724385ede13eb02d70ec2e7e540ce
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-04-21 23:27:33 +0900

    #69 update postRepository query

--- src/main/java/kr/seoul/snsX/repository/PostRepository.java
+++ src/main/java/kr/seoul/snsX/repository/PostRepository.java
@@ -9,7 +9,13 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(nativeQuery = true, value =
-    "SELECT p.post FROM post")
+            "SELECT post_id, thumbnail_file_name " +
+                    "FROM post " +
+                    "INNER JOIN (SELECT post_id FROM post LIMIT :offset, :limit) pp ON pp.post_id = post.post_id " +
+                    "WHERE post_id IN " +
+                    "(SELECT post_id FROM post_hash_tag WHERE hash_tag = " +
+                    "(SELECT hashtag_id FROM hash_tag WHERE name = :tagId))"
+    )
     List<Object[]> findPostIdAndThumbnailFileNameByTagId(@Param("tagId") Long tagId, @Param("offset") Long offset, @Param("limit") Long limit);
 
     @Query(nativeQuery = true, value = "SELECT * FROM post p JOIN (SELECT post_id FROM post ORDER BY post_id DESC LIMIT :offset, :limit) pp ON pp.post_id=p.post_id ORDER BY p.post_id DESC")
