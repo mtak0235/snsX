8c964d8e2aa636058d61884ca3ea46dc7b7625d2
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-04-22 00:08:34 +0900

    #68 #69 delete post_feed_form.html, post_feed_form -> post_feed_list, update post_feed_list

--- src/main/java/kr/seoul/snsX/controller/PostController.java
+++ src/main/java/kr/seoul/snsX/controller/PostController.java
@@ -107,7 +107,7 @@ public class PostController {
     @GetMapping("/")
     public String showFeedForm(@SessionAttribute(name = "userInfo", required = false) MemberInfoDto userInfo, Model model) {
         if (userInfo == null) {
-            return "post_feed_form";
+            return "post_feed_list";
         }
         model.addAttribute("member", userInfo);
         return "member_feed_form";
